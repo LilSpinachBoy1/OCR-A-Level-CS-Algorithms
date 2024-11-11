d16c855cf8e3785c655294c2b4339e9284fb4489
LilSPinachBoy1 <sam@moocows.co.uk>
2024-11-11 09:45:02 +0000

    Started python impl of linear search

---
+++ Searching/Linear Search/LinearSearch.py
@@ -0,0 +1,17 @@
+# Set up data
+data = ["Reese", "Advin", "Ralfs", "Josh", "Sam"]
+target = "Josh"
+
+length = len(data)                                  # Store the length of the data to use as an endpoint
+found = False                                       # Flag to improve efficiency
+i = 0                                               # Acts as a pointer to the position in the list
+while not found and i < length:                     # Keep looping while the target has not been found, and while the pointer is within the range of the list
+    if data[i] == target:                           # If the current item is the one to find
+        found = True                                # Set flag to true, so the loop ends
+    else:
+        i += 1                                      # Move to the next item if not found yet
+
+if found:
+    print("Item found at position " + str(i))
+else:
+    print("Item not found!")
