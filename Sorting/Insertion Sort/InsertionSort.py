e120774851d95a87fdbe7f0b1d9fc999f2a57571
LilSPinachBoy1 <sam@moocows.co.uk>
2024-11-12 12:37:17 +0000

    Started insertion sort

--- Sorting/Insertion Sort/InsertionSort.py
+++ Sorting/Insertion Sort/InsertionSort.py
@@ -1,3 +1,12 @@
 # Create dataset
 data = ["Wicker Man", "The Smiler", "Th13teen", "Rita", "Oblivion", "Nemesis", "Galactica"]
 
+#TODO: This is awful and wrong
+for index in range(1, len(data)):
+    temp_store = data[index]
+    checking = index - 1
+    while checking > 0 and data[checking] < data[index]:
+        data[index] = data[checking]
+        checking -= 1
+
+    data[checking]
