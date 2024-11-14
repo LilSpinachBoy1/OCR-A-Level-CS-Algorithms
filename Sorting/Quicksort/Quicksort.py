5b1b8a63bb05fb69cb7fe593ea819827a70e5c86
Sam <sam@moocows.co.uk>
2024-11-14 16:59:26 +0000

    I DID IT

--- Sorting/Quicksort/Quicksort.py
+++ Sorting/Quicksort/Quicksort.py
@@ -1,37 +1,39 @@
 # Create dataset
 data = ["Gouda", "Red Leicester", "Double Gloucester", "Parmesan", "Cheddar", "Brie", "Blue"]
-data = [4, 5, 2, 1, 3]
+
 
 def quicksort(items):
     if len(items) <= 1:
         return items
     else:
-        # Create neccesary pointers and swap pivot to end
+        # Create necessary pointers and swap pivot to end
         pivot = (len(items) - 1) // 2
-        data[-1], data[pivot] = data[pivot], data[-1]
+        items[-1], items[pivot] = items[pivot], items[-1]
         p1 = 0
         p2 = len(items) - 2
-        pivot_value = data[-1]
+        pivot_value = items[-1]
         pointer_swap = False
         while not pointer_swap:
             # Set p1: it should increment until >= pivot
-            while data[p1] < pivot_value:
+            while items[p1] < pivot_value:
                 p1 += 1
 
             # Set p2: it should decrement until < pivot
-            while data[p2] >= pivot_value:
+            while items[p2] >= pivot_value and p2 >= 0:
                 p2 -= 1
 
             # Find out if the pointers have swapped
             if p1 > p2:  # If the pointers have crossed, swap pivot and p1
                 pointer_swap = True
-                data[p1], data[-1] = pivot_value, data[p1]
+                items[p1], items[-1] = pivot_value, items[p1]
             else:  # If not, swap p1 and p2
-                data[p1], data[p2] = data[p2], data[p1]
-    left_partition = data[:p1]
-    right_partition = data[p1+1:]
+                items[p1], items[p2] = items[p2], items[p1]
+    left_partition = items[:p1]
+    right_partition = items[p1+1:]
     sorted_left = quicksort(left_partition)
     sorted_right = quicksort(right_partition)
-    return sorted_left + data[p1] + sorted_right
+    return sorted_left + [items[p1]] + sorted_right
 
-print(quicksort(data))
+
+sorted_data = quicksort(data)
+print(sorted_data)
